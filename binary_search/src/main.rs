7276ed5847d181d23ad0a3938ca9fe97c00738ba
Cristi Cretu <crisemcr@gmail.com>
2021-11-04 12:17:45 +0200

    Adds generics

--- binary_search/src/main.rs
+++ binary_search/src/main.rs
@@ -1,24 +1,33 @@
-fn binary_search(v1: &[i32], target: i32) -> usize {
+use std::cmp::Ordering;
+
+fn binary_search<T: Ord>(v1: &[T], target: &T) -> Option<usize> {
     let mut lb: usize = 0;
     let mut rb: usize = v1.len() - 1;
 
     while lb <= rb {
         let mb: usize = lb + (rb - lb) / 2;
 
-        if v1[mb] == target {
-            return mb;
-        } else if v1[mb] < target {
-            lb = mb + 1;
-        } else {
-            rb = mb - 1;
+        match target.cmp(&v1[mb]) {
+            Ordering::Less => rb = mb,
+            Ordering::Equal => return Some(mb),
+            Ordering::Greater => lb = mb + 1,
         }
     }
 
-    return 0;
+    None
 }
 
 fn main() {
     let vec = vec![1, 2, 3, 4, 5, 6, 7, 8];
 
-    println!("{}", binary_search(&vec, 6))
+    let index = binary_search(&vec, &6);
+
+    println!("{:?}", index); // use debugging mode for 'Option<T>
+                             // 'Some(x)' isn't suitable for end-user consumption
+
+    let vec2 = vec!["a", "b", "c", "d", "e"];
+
+    let index2 = binary_search(&vec2, &"c");
+
+    println!("{:?}", index2);
 }
