170ddcc50889b41a4105db2884d6c54ad2b44102
Cristi Cretu <crisemcr@gmail.com>
2021-11-14 20:38:27 +0200

    Event loop

--- snake_game/src/main.rs
+++ snake_game/src/main.rs
@@ -9,12 +9,39 @@ use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
 
+struct Game {
+    gl: GlGraphics,
+}
+
+impl Game {
+    fn render(&mut self, arg: &RenderArgs) {
+        use graphics;
+
+        let GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+
+        self.gl.draw(arg.viewport(), |c, gl| {
+            graphics::clear(GREEN, gl);
+        });
+    }
+}
+
 fn main() {
     let opengl = OpenGL::V3_2;
 
-    let mut window: Window = WindowSettings::new("Snake game", [200, 200])
+    let mut window: Window = WindowSettings::new("Window", [200, 200])
         .graphics_api(opengl)
         .exit_on_esc(true)
         .build()
         .unwrap();
+
+    let mut game = Game {
+        gl: GlGraphics::new(opengl),
+    };
+
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(r) = e.render_args() {
+            game.render(&r)
+        }
+    }
 }
